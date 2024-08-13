0622d186e502cb26d5f1a8b98ffac45edba94376
Srinjoy Sengupta <127625473+Srinjoy003@users.noreply.github.com>
2024-08-14 01:22:45 +0530

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define llt long long int
+
+int main() {
+    llt t;
+    cin >> t;
+
+    while (t--) {
+        llt n;
+        cin >> n;
+        vector<llt> A(n);
+
+        for (llt i = 0; i < n; i++) {
+            cin >> A[i];
+        }
+
+        cout << count << endl;
+    }
+
+    return 0;
+}

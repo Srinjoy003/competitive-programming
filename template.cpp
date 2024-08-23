0c487ac2b07a79b125392c8a4cc4782f23fbb9d9
Srinjoy Sengupta <127625473+Srinjoy003@users.noreply.github.com>
2024-08-23 17:00:15 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,6 +1,28 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 #define llt long long int
+#define MOD 1000000007
+
+#define debug(x) cerr << #x<<" "; _print(x); cerr << endl;
+
+void _print(llt t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(double t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
 
 int main() {
     llt t;

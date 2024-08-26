f10604cb98b4bc6b8ca06568683fc9631970ac48
Srinjoy Sengupta <127625473+Srinjoy003@users.noreply.github.com>
2024-08-27 02:59:46 +0530

    Update prefixSuffix.cpp

--- prefixSuffix.cpp
+++ prefixSuffix.cpp
@@ -8,3 +8,21 @@ vector <llt> calculatePrefix(vector <llt> arr) {
 
     return prefix;
 }
+
+
+vector<vector<llt>> computePrefixSum(const vector<vector<llt>> matrix) {
+    int m = matrix.size();
+    int n = matrix[0].size();
+    vector<vector<llt>> prefixSum(m, vector<llt>(n, 0));
+
+    for (int i = 0; i < m; ++i) {
+        for (int j = 0; j < n; ++j) {
+            prefixSum[i][j] = matrix[i][j]
+                + (i > 0 ? prefixSum[i-1][j] : 0)
+                + (j > 0 ? prefixSum[i][j-1] : 0)
+                - (i > 0 && j > 0 ? prefixSum[i-1][j-1] : 0);
+        }
+    }
+
+    return prefixSum;
+}

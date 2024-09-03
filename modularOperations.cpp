ef2337fec32406d38289315af914e0300a0b8644
Srinjoy Sengupta <127625473+Srinjoy003@users.noreply.github.com>
2024-09-03 21:45:21 +0530

    Update modularOperations.cpp

--- modularOperations.cpp
+++ modularOperations.cpp
@@ -9,9 +9,26 @@ llt mod_exp(llt base, llt exp, llt mod) {
     }
     return result;
 }
+llt modInverse(llt a, llt mod) {
+    return mod_exp(a, mod - 2, mod);
+}
 
 llt modMultiply(llt a, llt b) {
-    return ((a % MOD) * (b % MOD) % MOD;
+    return ((a % MOD) * (b % MOD)) % MOD;
+}
+
+llt binomialCoeff(llt n, llt k) {
+    if (k > n - k) 
+        k = n - k;
+ 
+    llt res = 1;
+
+    for (llt i = 0; i < k; ++i) {
+        res = modMultiply(res, n - i);
+        res = modMultiply(res, modInverse(i + 1, MOD));
+    }
+ 
+    return res;
 }
 
 llt modSum(llt a, llt b) {
